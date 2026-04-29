06d448b9b542d19349fb23cb79dd5d97dd1ded95
naqiuddinnazali-lang <naqiuddin.nazali@dyson.com>
2026-04-29 14:59:20 +0800

    Updated pipeline

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
+import json
 from g3_scrape import get_asian_countries
 from g3_excel import save_to_excel
-#from g3_condition import
-#from g3_db import
+from g3_condition import get_top_10_countries
+from g3_db import int_db, save_db
 
 def run_pipeline():
     countries = get_asian_countries()
@@ -10,6 +11,11 @@ def run_pipeline():
         print(c)
         file_path = save_to_excel(c)
         print("Excel file saved to:", file_path)
+
+    int_db()
+    data = get_top_10_countries()
+    record = save_db(data)
+    print(json.dumps(record, indent=2))
 
     
 
