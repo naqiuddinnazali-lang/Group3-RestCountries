e564796f55f9ceb8886218c979e3749a5024b109
naqiuddinnazali-lang <naqiuddin.nazali@dyson.com>
2026-04-29 16:03:49 +0800

    Added Telegram Call

--- g3_telegram.py
+++ g3_telegram.py
@@ -11,9 +11,9 @@ TELEGRAM_CHAT_ID   = os.getenv("TELEGRAM_CHAT_ID")
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
 
 def send_telegram_notification(record: dict) -> bool:
-    population    = record["data"]
+    #population    = record["data"]
     #record_id  = record["id"]
-    created_at = record["created_at"]
+    #created_at = record["created_at"]
 
     message = (
         f"<b>Highest Population Country Updated</b>\n"
